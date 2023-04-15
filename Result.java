53b4a4443990af69b98b5cb64f480361d5bdcf94
GautamGupta0109 <108135402+GautamGupta0109@users.noreply.github.com>
2023-04-15 14:19:39 +0530

    Add files via upload

---
+++ Result.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Result {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in); 
+            System.out.print("Enter the marks: ");
+            int marks = sc.nextInt();
+            if(marks>90)
+            {
+                System.out.println("Excellent");
+            }
+            else if(marks>80 && marks<=90)
+            {
+                System.out.println("Good");
+
+            }
+            else if(marks>70 && marks<=80)
+            {
+                System.out.println("Fair");
+            }
+            else if(marks>60 && marks<=70)
+            {
+                System.out.println("Improve yourself ");
+            }
+            else 
+            {
+                System.out.println("FAIL");
+            }
+            sc.close();
+    }
+}
